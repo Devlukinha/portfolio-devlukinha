849f1504e92fc14b2cd5901472b6b46eab85f5a4
Lucas Henrique <167331612+Devlukinha@users.noreply.github.com>
2024-11-02 21:18:20 -0300

    Correção no Javascript

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
+let btnMenu = document.getElementById('btn-menu');
+let menu = document.getElementById('menu-mobile');
+
+btnMenu.addEventListener('click', ()=>{
+    menu.classList.add('abrir-menu')
+})
+
+menu.addEventListener('click', ()=>{
+    menu.classList.remove('abrir-menu')
+})
+
 let logo = document.getElementById("logo-cabecalho");
 
 logo.addEventListener('click', () =>{
     alert("Você achou um easter egg");
 });
-
-
-
-
-
